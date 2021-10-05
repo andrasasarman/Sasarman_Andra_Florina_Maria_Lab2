9ea46fb9ba6b00f9a30cbfe33cff96ed1aea8d7f
andra <andra@DESKTOP-JHR6NHP>
2021-10-05 20:12:27 +0300

    Add project files.

---
+++ Sasarman_Andra_Florina_Maria_Lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sasarman_Andra_Florina_Maria_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
